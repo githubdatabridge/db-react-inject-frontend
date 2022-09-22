0e5f097ab1cde620aef07b6d958c2a4bfef4be09
Patric Amatulli <patric.amatulli@databridge.ch>
2022-09-22 11:47:05 +0200

    fix: removed .lock file

---
+++ build/env.js
@@ -0,0 +1,6 @@
+window.env = {
+  REACT_APP_BG_COLOR: "white",
+  REACT_APP_MAIN_TEXT: "HELLO WHITE",
+  REACT_APP_LINK: "www.9gag.com",
+  REACT_APP_LOGO: "https://fraguru.com/mdimg/dizajneri/o.4737.jpg",
+};
